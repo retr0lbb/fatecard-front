687292c63753c154fc28a69382b05cd1a8625c0c
GPatriota9 <bielpatriota@hotmail.com>
2025-11-08 19:56:35 -0300

    feat: add smes

--- src/router/router.tsx
+++ src/router/router.tsx
@@ -1,10 +1,12 @@
 import { Route, Routes } from "react-router";
 import Page from "../pages/main"
+import CriarPalestra from "../cadastropalestra";
 
 export function MyRouter(){
     return(
     <Routes>
         <Route path="/" element={<Page />} />
+        <Route path="/add" element={<CriarPalestra />} />
     </Routes>
     )
 }
