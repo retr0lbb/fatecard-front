999bfd9ca9573caee988069f04b54a116140c87a
GPatriota9 <bielpatriota@hotmail.com>
2025-11-08 19:40:05 -0300

    nothing

--- src/router/router.tsx
+++ src/router/router.tsx
@@ -1,12 +1,13 @@
 import { Route, Routes } from "react-router";
 import { App } from "../App";
+import CriarPalestra from "../cadastropalestra";
 
 
 
 export function MyRouter(){
     return(
     <Routes>
-        <Route path="/" element={<App />} />
+        <Route path="/" element={<CriarPalestra />} />
     </Routes>
     )
 }
